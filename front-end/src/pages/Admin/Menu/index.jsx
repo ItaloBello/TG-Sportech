1c0ee32b5e0773be0d9aca8c3340b82c76f6c0af
ARMCardoso <cardosoandre330@gmail.com>
2025-05-09 16:40:47 -0300

    Atualização de endpoints de Admin

--- front-end/src/pages/Admin/Menu/index.jsx
+++ front-end/src/pages/Admin/Menu/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../../components/Header'
 import MenuItem from '../../../components/MenuItem'
+import { useAdminAuth } from '../../../hooks/useAdminAuth'
 
 const AdminMenu = () => {
+  const { admin } = useAdminAuth();
   return (
     <div className='admin-menu'>
         <Header/>
+        <p>Admin : {admin.name} </p>
         <MenuItem label='Quadras' link='/admin/court-menu'/>
         <MenuItem label='Campeonatos' link='/admin/championship-menu'/>
         <MenuItem label='Agendamentos'/>
