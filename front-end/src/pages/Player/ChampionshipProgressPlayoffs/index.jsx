2cfbea8fc006c76207b532ffca0b7dfb1369da6f
ItaloBello <italo.bello10@gmail.com>
2025-05-17 15:25:40 -0300

    inicio tela de progresso playoff

--- front-end/src/pages/Player/ChampionshipProgressPlayoffs/index.jsx
+++ front-end/src/pages/Player/ChampionshipProgressPlayoffs/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../../../components/Header'
 
 const ChampionshipProgress = () => {
   return (
     <div className='championship-in-progress'>
-      
+      <Header/>
+      <div className='progress-card'>
+
+      </div>
     </div>
   )
 }
