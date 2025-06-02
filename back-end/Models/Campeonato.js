a91a829fc6d7854e4e5fafb24b9fdcd44920fc06
Gabriel Jonathan <gabrielpadawan912@gmail.com>
2025-06-02 00:09:09 -0300

    adição dos endpoints de criação de campeonato e de confirmação do pagamento

---
+++ back-end/Models/Campeonato.js
@@ -0,0 +1,51 @@
+// Conecta no Banco
+const Sequelize = require('sequelize')
+const sequelize = require('../database')
+const DonoQuadra = require('./DonoQuadra');
+const Quadra = require('./Quadra');
+
+//Model(tabela sem comando sql) - Usuarios
+const Campeonato = sequelize.define('campeonato',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+    },
+    registro: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+    },
+    data_inicio: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    num_times: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    descricao: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    premiacao: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "não iniciado"
+    }
+}, {
+    timestamps: false 
+})
+
+Campeonato.belongsTo(DonoQuadra);
+DonoQuadra.hasMany(Campeonato);
+
+module.exports = Quadra;
--- back-end/database.js
+++ back-end/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('SPORTECH', 'postgres', 'admin', {
+const sequelize = new Sequelize('SPORTECH', 'postgres', 'Admin', {
     host: "localhost",
-    port: 5432,
+    port: 5433,
     dialect: 'postgres',
     logging: false,
     pool: {
